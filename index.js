1e5f3954fdf20dbcf118138105bbabedd8ca2e29
Matheus Souza <matheusmargarido.contato@gmail.com>
2024-04-04 14:08:09 -0300

    Glossario

---
+++ index.js
@@ -0,0 +1,26 @@
+db = {};
+
+db.init = function(callback) {
+    $.getJSON('db/db.json', function(data) {
+        db.data = data;
+        if(callback) {
+            callback();
+        }
+    });
+}
+
+initSelect = function() {
+    for (var i = 0; i < db.data.length; i++) {
+        var item = db.data[i];
+        var select_text = "<option value='" + item.id + "'>" + item.titulo + "</option>";
+        $('#slc-topicos').append(select_text);
+    }
+};
+
+onSelectChange = function() {
+    var id = $('#slc-topicos').val();
+    var item = db.data[id];
+    $('#txt-desc').text(item.descricao);
+};
+
+db.init(initSelect);
